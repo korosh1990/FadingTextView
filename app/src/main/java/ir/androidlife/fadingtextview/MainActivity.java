0b7788032ad97a6f517d41666364c4b6954b5e52
Korosh1990 <saberikorosh@gmail.com>
2018-08-02 08:56:59 -0700

    Initial commit

---
+++ app/src/main/java/ir/androidlife/fadingtextview/MainActivity.java
@@ -0,0 +1,15 @@
+package ir.androidlife.fadingtextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/irsans.ttf");
+    }
+}
